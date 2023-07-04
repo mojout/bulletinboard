37f2a5cc91f18b45d595004fa31fdea33f38c45e
Dmitry Zaharov <98744363+Dimose75@users.noreply.github.com>
2023-07-04 13:02:24 +0400

    board: create views

--- bulletinboard/settings.py
+++ bulletinboard/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
